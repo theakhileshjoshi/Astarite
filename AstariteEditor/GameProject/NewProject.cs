f9565ae2ecd4bc70580d76a3d713c30e59d0b58a
Akhilesh Joshi <theakhileshjoshi@gmail.com>
2023-08-13 23:19:00 +0530

    Project Dialog Created

---
+++ AstariteEditor/GameProject/NewProject.cs
@@ -0,0 +1,37 @@
+﻿using AstariteEditor.Resource;
+using System;
+
+namespace AstariteEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = StringResource.DefaultProjectName;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\AstariteProject\";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+    }
+}
---
+++ AstariteEditor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AstariteEditor.GameProject;
+using System.Windows;
+
+namespace AstariteEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            OpenProjectBrowserDialog();
+            Loaded -= OnMainWindowLoaded;
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowser = new ProjectBrowserDialog();
+            if (projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
