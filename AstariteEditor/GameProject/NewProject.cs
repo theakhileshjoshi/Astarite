4a00552e4a46f68be74940311c9fdc70371a89ac
Akhilesh Joshi <theakhileshjoshi@gmail.com>
2023-08-18 00:33:10 +0530

    Template xml serializer utility created!

--- AstariteEditor/GameProject/NewProject.cs
+++ AstariteEditor/GameProject/NewProject.cs
@@ -1,10 +1,30 @@
 ﻿using AstariteEditor.Resource;
+using AstariteEditor.Utilities;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
 
 namespace AstariteEditor.GameProject
 {
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string? ProjectType { get; set; }
+        [DataMember]
+        public string? ProjectFile { get; set; }
+        [DataMember]
+        public List<string>? Folders { get; set; }
+
+    }
+
     class NewProject : ViewModelBase
     {
+        //TODO: Get the path from the installation location
+        private readonly string _templatePath = @"..\..\AstariteEditor\ProjectTemplates";
         private string _name = StringResource.DefaultProjectName;
         public string Name
         {
@@ -32,6 +52,32 @@ namespace AstariteEditor.GameProject
                     OnPropertyChanged(nameof(Path));
                 }
             }
+        }
+
+        public NewProject()
+        {
+            try
+            {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Any());
+                foreach (var templatesFile in templatesFiles)
+                {
+                    var template = new ProjectTemplate()
+                    {
+                        ProjectType = "Empty Project",
+                        ProjectFile = "project.astarite",
+                        Folders = new List<string> { ".Astarite", "Content", "GameCode" }
+                    };
+
+                    Serializer.ToFile(template, templatesFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: Log error
+            }
+
         }
     }
 }
---
+++ AstariteEditor/Utilities/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace AstariteEditor.Utilities
+{
+    public static class Serializer
+    {
+        public static void ToFile<T>(T instance, string path)
+        {
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(fs, instance);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: Proper Logging
+            }
+        }
+    }
+}
